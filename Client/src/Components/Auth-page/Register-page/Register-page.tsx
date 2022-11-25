aa7576029b92cf0054e1ad062ca24059184eb958
Evgeniy <divizia21@bk.ru>
2022-11-25 17:43:00 +0300

    split-auth-page

--- Client/src/Components/Auth-page/Register-page/Register-page.tsx
+++ Client/src/Components/Auth-page/Register-page/Register-page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuthQuery } from '../../../redux/Endpoints/Auth-endpoints'
-import style from './Register-page.module.scss'
+import { StandardAuthPage } from '../Standard-auth-page'
+// import style from './Register-page.module.scss'
 
 export const RegisterPage: React.FC = () => {
   const { data }: any = useAuthQuery({})
   if (!data?.auth) {
-    return <div className={style.RegisterPage}>registration</div>
+    return <StandardAuthPage linkTo="login" />
   } else {
     return <Navigate to={'/'} />
   }
